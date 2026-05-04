421a37c686f7ccf0e27acae8b2eddb90e154ffcf
SyntaxxErr0r1 <murad_turkmen96@hotmail.com>
2026-05-04 22:03:53 +0200

    fix

--- create_users.sh
+++ create_users.sh
@@ -1,16 +1,16 @@
-#! /bin/bash
+#!/bin/bash
 
-# kolla om man är root
+# kontrollera att scriptet körs som root
 if [ "$EUID" -ne 0 ]
 then
 echo "Du måste vara root för detta!"
 exit 1
 fi
 
-#kolla om man skrev in några namn
+#validera argument (minst ett användarnamn skickats in)
 if [ $# -eq 0 ]
 then
-echo "Du glömde  skriva namn. Ange  ./create_users.sh namn1 namn2"
+echo "Du glömde skriva namn. Ange  ./create_users.sh namn1 namn2"
 exit 1
 fi
 
@@ -19,7 +19,7 @@ for namn in $@
 do
   echo "Nu fixar vi användaren: $namn"
 
-  # Skapa användaren och mappen /home/namn
+  # Skapa systemanvändare och home directory
   useradd -m $namn
 
   # skapa de tre mapparna
@@ -27,15 +27,13 @@ do
   mkdir /home/$namn/Downloads
   mkdir /home/$namn/Work
 
-  #sätt rättigheter på det enklaste sättet:
-  # u=rwx (user får läsa/skriva/köra)
-  #ggo= (Group och 0thers får ingeting)
-  chmod u=, go= /home$namn/Documents
-  chmod u=, go= /home$namn/Downloads
-  chmod u=, go= /home$namn/Work
+  # sätt rättigheter: full åtkomst för ägaren, blockera grupp och övriga
+  chmod 700 /home/$namn/Documents
+  chmod 700 /home/$namn/Downloads
+  chmod 700 /home/$namn/Work
 
   # Skapa välkkomstfilen
-  echo "välkommen $namn" > /home/$namn/welcome.txt
+  echo "Välkommen $namn" > /home/$namn/welcome.txt
   echo "Här är alla andra användare:" >> /home/$namn/welcome.txt
 
   # Hämta listan på alla användare (enklaste sättet)
