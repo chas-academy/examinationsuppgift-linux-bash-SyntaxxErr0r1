cfbda585f416b3b4f99604db1d2b746c639def54
SyntaxxErr0r1 <murad_turkmen96@hotmail.com>
2026-05-05 02:43:41 +0200

    update

--- create_users.sh
+++ create_users.sh
@@ -7,32 +7,16 @@ then
   exit 1
 fi
 
-# Validera argument (minst ett användarnamn skickats in)
-if [ $# -eq 0 ]
-then
-  echo "Du glömde skriva namn. Ange ./create_users.sh namn1 namn2"
-  exit 1
-fi
-
-# Här börjar loopen för alla namn
+# Här börjar loopen för alla argument
 for namn in "$@"
 do
   echo "Nu fixar vi användaren: $namn"
 
-  # Kontrollera om användaren redan finns
-  
-  if id "$namn" &>/dev/null; then
-    echo "Användaren $namn finns redan, Hoppa över."
-    continue
-  fi
-
-  # 1. Hämta listan över befintliga användare INNAN den nya skapas
-  existing_users=$(cut -d: -f1 /etc/passwd)
-
-  # 2. Skapa användaren
+  # skapa användaren med hemkatalog
   useradd -m "$namn"
 
-  # 3. Skapa mappar 
+  # skapa undermappar i hemkatalogen
+
   mkdir -p "/home/$namn/Documents" "/home/$namn/Downloads" "/home/$namn/Work"
 
   # 4. Sätt rättigheter (Endast ägaren)
@@ -40,10 +24,18 @@ do
 
   # 5. Skapa välkomstfilen
   echo "Välkommen $namn" > "/home/$namn/welcome.txt"
-  echo "$existing_users" >> "/home/$namn/welcome.txt"
-  
-  # 6. Ge användaren ägarskap över sin hemkatalog
-  chown -R "$namn:$namn" "/home/$namn"
+ 
+  #Lägg till en lista på alla andra användare
 
-  echo "$namn är klar!"
-done
+  echo "Andra användare:" >> "/home/$namn/welcome.txt"
+  cut -d: -f1 /etc/passwd >> "home/$namn/welcome.txt"
+
+  # Ge användaren ägarskap över sin hemkatalog
+  chown -R "$namn" "/home/$namn"
+
+  done
+
+  echo "allt är klart!"
+  
+
+  
